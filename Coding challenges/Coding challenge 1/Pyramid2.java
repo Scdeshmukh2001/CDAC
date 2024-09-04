72a78851f5ed2643595141fe8d6b3c9613011fe2
Sumit Deshmukh <90791914+Scdeshmukh2001@users.noreply.github.com>
2024-09-04 17:33:36 +0530

    Add files via upload

---
+++ Coding challenges/Coding challenge 1/Pyramid2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.Scanner;
+
+public class Pyramid2 {
+    public static void main(String[] args) {     
+
+        for (int i = 7; i >= 1; i--) {
+            // Print leading spaces
+            for (int j = 7; j > i; j--) {
+                System.out.print(" ");
+            }
+            // Print stars and spaces for hollow inverted pyramid
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                if (j == 1 || j == (2 * i - 1) || i == 1 || i == 7) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
